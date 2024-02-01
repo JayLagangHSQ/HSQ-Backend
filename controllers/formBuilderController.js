714b26f5ab212f462d81bb463004ddbd72634851
Marvin Jay Lagang <marvin.lagang@hollywoodsq.com>
2024-02-02 01:04:07 +0800

    Created the API to add newField to an existing form/questionnaire	and Created the API to delete a specific to an existing form/questionnaire

--- controllers/formBuilderController.js
+++ controllers/formBuilderController.js
@@ -1,11 +1,13 @@
 const Questionnaire = require('../models/Questionnaire');
+const fieldOptions = require('../util/fieldOptions')
 
 module.exports.createNewForm = async(req, res) => {
 
-const { id } = req.user;
-const creator = id;
-let { title, description, fields } = req.body;
-let editor = [];
+    const { id } = req.user;
+    const creator = id;
+    let { title, description, fields } = req.body;
+    let editor = [];
+    
     if (!title || title === ""){
         title = "Untitled"
     }
@@ -23,13 +25,65 @@ let editor = [];
         fields
     });
     await form.save();
-    res.status(200).json({
-        message: "Form created successfully",
-        form
-    });
+    return res.status(200).json(form);
 }
 
-module.exports.editForm = async(req,res) =>{
+module.exports.addField = async (req, res) => {
+    const selectedField = req.body.fieldType
+    const newField = fieldOptions[selectedField]
+    try {
+        const questionnaire = req.questionnaire
+        if((selectedField+1) > fieldOptions.length || (!newField)){
+            return res.status(400).json({error: 'selected field is not valid'});
+        }
+        
+        questionnaire.fields.push(newField)
+        questionnaire.lastSavedTime = new Date()
+    
+        await questionnaire.save();
+    
+        return res.status(200).json(questionnaire);
+
+    } catch(err){
+        return res.status(500).send({error: 'internal error'})
+    }
+}
+
+module.exports.deleteField = async(req,res) =>{
+    const {index} = req.body
+    try {
+        const questionnaire = req.questionnaire
+        if(!questionnaire){
+            return res.status(404).json({error: `form not found`});
+        }
+        if(typeof index !== 'number'){
+            return res.status(400).json({error: `invalid index`});
+        }
+        if(!questionnaire.fields[index]){
+            return res.status(400).json({error: `No found field at index ${index}`});
+        }
+
+        questionnaire.fields.splice(index, 1)
+        questionnaire.lastSavedTime = new Date()
+    
+        await questionnaire.save();
+    
+        return res.status(200).json(questionnaire);
+
+    } catch(err){
+        return res.status(500).send({error: 'internal error'})
+    }
+}
+
+module.exports.addChoiceOption = async(req,res) =>{
+
+}
+
+module.exports.addCandidateOption = async(req,res) =>{
+
+}
+
+module.exports.editField = async(req,res) =>{
 
 }
 
@@ -39,4 +93,10 @@ module.exports.queryForms = async(req,res) =>{
 
 module.exports.retrieveForm = async(req,res) =>{
     
+}
+module.exports.collateResponses =async(req, res) =>{
+
+}
+module.exports.addEditor = async (req,res) =>{
+
 }
--- routes/formBuilder.js
+++ routes/formBuilder.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const formBuilderController = require('../controllers/formBuilderController')
 const auth = require('../auth')
+const questionnaireAuth = require('../middlewares/questionnaireAuth')
 const {verify}  = auth;
+const {verifyEditAuthorization} = questionnaireAuth
 
 const router = express.Router();
 
 router.post('/newForm', verify, formBuilderController.createNewForm);
-router.put('/:questionnaireId/edit', verify, formBuilderController.editForm)
-router.post('/query', verify)
+router.put('/:questionnaireId/newField', verify,verifyEditAuthorization, formBuilderController.addField)
+router.put('/:questionnaireId/deleteField', verify,verifyEditAuthorization, formBuilderController.deleteField)
+router.put('/:questionnaireId/edit', verify, formBuilderController.editField)
+// router.post('/query', verify, formBuilderController)
 
 module.exports = router;
